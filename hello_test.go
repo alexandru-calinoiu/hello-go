370e718ec5bd1257c0a560d959207557ce4248e3
calin <calinoiu.alexandru@agilefreaks.com>
2021-03-22 22:52:58 +0200

    Improve greater

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Calin")
-	want := "Hello, Calin"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("say hellop to people", func(t *testing.T) {
+		got := Hello("Calin")
+		want := "Hello, Calin"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 }
