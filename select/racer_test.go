0483c78f8c76aa2f1590e1fecefa9cea55f1d475
calin <calinoiu.alexandru@agilefreaks.com>
2021-03-28 06:16:50 +0300

    More concurency

---
+++ select/racer_test.go
@@ -0,0 +1,45 @@
+package _select
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	t.Run("return he fastest site", func(t *testing.T) {
+		slowServer := makeDelayedServer(20)
+		defer slowServer.Close()
+		fastServer := makeDelayedServer(0)
+		defer fastServer.Close()
+
+		slowURL := slowServer.URL
+		fastUrl := fastServer.URL
+
+		want := fastUrl
+		got, _ := Racer(slowURL, fastUrl)
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+	t.Run("returns an error if a server doesn't respond with 10s", func(t *testing.T) {
+		server := makeDelayedServer(25 * time.Millisecond)
+		defer server.Close()
+
+		_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
+
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
+
+	})
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
